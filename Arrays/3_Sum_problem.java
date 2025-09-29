ce5cae7c0bd4d90df5e5dd189c4a93a3ecd71cf2
Asit_Sengar <asitsengar28@gmail.com>
2025-09-29 19:12:22 +0530

    Update 3_Sum_problem.java

--- Arrays/3_Sum_problem.java
+++ Arrays/3_Sum_problem.java
@@ -1,35 +1,40 @@
-import java.util.List;
-import java.util.Collections;
-import java.util.Set;
 import java.util.ArrayList;
-import java.util.HashSet;  
+import java.util.Arrays;
+import java.util.List;
 
 class Solution {
     public List<List<Integer>> threeSum(int[] arr) {
+        Arrays.sort( arr);
         List<List<Integer>> list = new ArrayList<>();
-        Set<List<Integer>> myset = new HashSet<>();
-        for (int i = 0; i < arr.length; i++)
-        {
-            Set<Integer> setinteger = new HashSet<>();
-            for (int j = i + 1; j < arr.length; j++) {
-                List<Integer> temp = new ArrayList<>();
-                int k = -(arr[i] + arr[j]);
-                if (setinteger.contains(k)) {
+        for (int i = 0; i <= arr.length - 3; i++) {
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                continue;
+            }
+            int j = i + 1;
+            int k = arr.length - 1;
+            while (j < k) {
+                int sum = arr[i] + arr[j] + arr[k];
+                if (sum < 0) {
+                    j = j + 1;
+                } else if (sum > 0) {
+                    k = k - 1;
+                } else {
+                    List<Integer> temp = new ArrayList<>();
                     temp.add(arr[i]);
                     temp.add(arr[j]);
-                    temp.add(k);
-                    Collections.sort( temp);
-                    {
-                        myset.add( temp);
+                    temp.add(arr[k]);
+                    j = j + 1;
+                    k = k - 1;
+                    list.add(temp);
+                    while (j < k && arr[j] == arr[j - 1]) {
+                        j = j + 1;
                     }
-                }
-                else
-                {
-                    setinteger.add(arr[j]);
+                    while (j < k && arr[k] == arr[k + 1]) {
+                        k = k - 1;
+                    }
                 }
             }
         }
-        list.addAll(myset);
         return list;
     }
 }
